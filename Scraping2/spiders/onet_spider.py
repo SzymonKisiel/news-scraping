7448c177d5a458cb46b8918f18177f6b9a137cfe
skisiel <szymon.kisiel99@gmail.com>
2022-04-04 14:12:45 +0200

    Fix onet.pl crawling

--- Scraping2/spiders/onet_spider.py
+++ Scraping2/spiders/onet_spider.py
@@ -1,32 +1,33 @@
+# response.css(".archiveDay ::attr(href)").getall()
+# response.css(".itemTitle ::attr(href)").getall()
+
 import scrapy
 
 
 class OnetNewsSpider(scrapy.Spider):
     name = "onet_spider"
-    page = 1
-    MAX_PAGE = 10
     start_urls = [
-        'https://wiadomosci.onet.pl',
-        'https://wiadomosci.onet.pl/?ajax=1&page=1'
+        'https://wiadomosci.onet.pl/mapa-serwisu'
     ]
 
     def parse(self, response):
-        articles = response.css(".mediumNewsBox::attr(href), .itemBox::attr(href)").getall()
+        archive_days = response.css(".archiveDay ::attr(href)").getall()
+        yield from response.follow_all(archive_days, self.parse_day)
+
+    def parse_day(self, response):
+        articles = response.css(".itemTitle ::attr(href)").getall()
         yield from response.follow_all(articles, self.parse_article)
-        if self.page < self.MAX_PAGE:
-            self.page += 1
-            url = f"https://wiadomosci.onet.pl/?ajax=1&page={self.page}"
-            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse_article(self, response):
         def extract_with_css(query):
             return response.css(query).get(default='').strip()
 
+        # returns text for first matched query
         def extract_all_with_css(*queries):
             for query in queries:
                 result = ''
                 for block in response.css(query).getall():
-                    result += block.strip()
+                    result += block.strip() + " "
                 if result != '':
                     return result
 
