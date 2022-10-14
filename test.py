28e146711cf57df1f4e7d931707bba771b1137f2
skisiel <szymon.kisiel99@gmail.com>
2022-10-14 20:13:37 +0200

    Updated test.py

--- test.py
+++ test.py
@@ -1,38 +1,12 @@
 import datetime
-from itertools import count
-from sys import stdin
-
-import scrapy.settings
-from twisted.internet import reactor
-
 from utils.time_util import string_to_datetime, get_timezone_aware_now
-from time import sleep
-from twisted.internet import reactor, defer, task
-import signal
-
-from scrapy.crawler import CrawlerRunner, CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from Scraping2.spiders.fakt_spider import FaktNewsSpider
-from Scraping2.spiders.onet_spider import OnetNewsSpider
-from Scraping2.spiders.radiozet_spider import RadiozetNewsSpider
-from Scraping2.spiders.rmf24_spider import Rmf24NewsSpider
-from Scraping2.spiders.tvn24_spider import Tvn24NewsSpider
-from scrapy.exceptions import CloseSpider
-
-import logging
-import pytz
-
-from multiprocessing import Process, Queue
+from twisted.internet import defer
+from scrapy.crawler import CrawlerProcess
 from reactor_queue import ReactorQueue
-
-from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
-from scrapy.utils.log import configure_logging
 from twisted.internet import reactor
-
 from Scraping2.spiders.test_spider1 import AuthorSpider
 from Scraping2.spiders.test_spider2 import QuotesSpider
-
 from scrapy.settings import Settings
 from scrapy import Spider
 
@@ -41,48 +15,21 @@ def test():
     print("Test started")
 
     websites = ['authors', 'quotes']
-    crawl_count = 2
-    # crawl_websites(websites)
+
     settings = get_project_settings()
     settings.set("FEEDS", {
         f"data/test_fajne/authors.jsonl": {"format": "jsonlines", "encoding": "utf8"},
     })
     settings.set("LOG_ENABLED", False)
-    runner = CrawlerRunner(settings)
 
     settings2 = get_project_settings()
     settings2.set("FEEDS", {
         f"data/test_fajne/quotes.jsonl": {"format": "jsonlines", "encoding": "utf8"},
     })
     settings2.set("LOG_ENABLED", False)
-    runner2 = CrawlerRunner(settings2)
 
-    ###############################################
-    # runner.crawl(AuthorSpider)
-    # runner2.crawl(QuotesSpider)
-    #
-    # d = runner.join()
-    # d.addCallback(30, run_spider)
-
-    ###############################################
-    # @defer.inlineCallbacks
-    # def test_crawl(i=0):
-    #     print(f"Test crawl {i}")
-    #     yield runner.crawl(AuthorSpider)
-    #     yield runner2.crawl(QuotesSpider)
-    #     reactor.callLater(10, test_crawl, i + 1)
-    #     reactor.stop()
-
-    # test_crawl()
-    # reactor.run()
-
-    ###############################################
-    # run_test1()
-    # run_test2()
-    # reactor.run()
-
-    ###############################################
     queue = ReactorQueue(2)
+
     # run infinitely
     # run_wrapper(AuthorSpider, queue, settings, 4)
     # run_wrapper(QuotesSpider, queue, settings2, 6)
@@ -102,8 +49,8 @@ def test():
     # run_wrapper3(QuotesSpider, queue, settings2, 6, due_time2)
 
     # run to due time
-    # due_time1 = string_to_datetime("2022-10-13T16:52:00+0200")
-    # due_time2 = string_to_datetime("2022-10-13T16:54:00+0200")
+    # due_time1 = string_to_datetime("2022-10-14T18:00:00+0200")
+    # due_time2 = string_to_datetime("2022-10-14T17:50:00+0200")
     # run_wrapper3(AuthorSpider, queue, settings, 20, due_time1)
     # run_wrapper3(QuotesSpider, queue, settings2, 30, due_time2)
 
@@ -112,31 +59,28 @@ def test():
     print("Test finished")
 
 
-# def run_test1(i=0):
-#     print(f"[{datetime.datetime.now()}] Test1 run {i}")
-
-#     settings = get_project_settings()
-#     settings.set("FEEDS", {
-#         f"data/test_fajne/authors.jsonl": {"format": "jsonlines", "encoding": "utf8"},
-#     })
-#     settings.set("LOG_ENABLED", False)
+# @defer.inlineCallbacks
+# def run(spider: Spider,
+#         queue: ReactorQueue,
+#         settings: Settings,
+#         delay: int = 60,
+#         i: int = 0,
+#         crawl_count: int = None,
+#         due_time: datetime = None):
+#     print(f"[{datetime.datetime.now().strftime('%m/%d/%Y, %H:%M:%S')}] {spider.name} run {i}")
+#
 #     runner = CrawlerProcess(settings)
-#     runner.crawl(AuthorSpider)
-
-#     reactor.callLater(10, run_test1, i + 1)
-
-
-# def run_test2(i=0):
-#     print(f"[{datetime.datetime.now()}] Test2 run {i}")
-
-#     settings = get_project_settings()
-#     settings.set("FEEDS", {
-#         f"data/test_fajne/quotes.jsonl": {"format": "jsonlines", "encoding": "utf8"},
-#     })
-#     settings.set("LOG_ENABLED", False)
-#     runner = CrawlerProcess(settings)
-#     runner.crawl(QuotesSpider)
-#     reactor.callLater(20, run_test2, i + 1)
+#     yield runner.crawl(spider)
+#
+#     now = get_timezone_aware_now()
+#     now_delayed = now + datetime.timedelta(seconds=delay)
+#     reached_due_time = now_delayed > due_time if due_time is not None else False
+#     reached_max_count = i + 1 >= crawl_count if crawl_count is not None else False
+#
+#     if not reached_due_time and not reached_max_count:
+#         reactor.callLater(delay, run, spider, queue, settings, delay, i + 1, crawl_count, due_time)
+#     else:
+#         queue.pop()
 
 
 @defer.inlineCallbacks
@@ -147,19 +91,34 @@ def run(spider: Spider,
         i: int = 0,
         crawl_count: int = None,
         due_time: datetime = None):
-    print(f"[{datetime.datetime.now().strftime('%m/%d/%Y, %H:%M:%S')}] {spider.name} run {i}")
 
+    # check conditions
+    if due_time is not None:
+        now_delayed = get_timezone_aware_now() + datetime.timedelta(seconds=delay)
+        reached_due_time = now_delayed > due_time
+    else:
+        reached_due_time = False
+
+    if crawl_count is not None:
+        reached_max_count = i + 1 >= crawl_count
+    else:
+        reached_max_count = False
+
+    reached_any_end_condition = reached_due_time or reached_max_count
+
+    # schedule next crawl
+    if not reached_any_end_condition:
+        reactor.callLater(delay, run, spider, queue, settings, delay, i + 1, crawl_count, due_time)
+
+    # log crawl start
+    print(f"[{datetime.datetime.now().strftime('%m/%d/%Y, %H:%M:%S.%f')}] {spider.name} run {i}")
+
+    # run crawler
     runner = CrawlerProcess(settings)
     yield runner.crawl(spider)
 
-    now = get_timezone_aware_now()
-    now_delayed = now + datetime.timedelta(seconds=delay)
-    reached_due_time = now_delayed > due_time if due_time is not None else False
-    reached_max_count = i + 1 >= crawl_count if crawl_count is not None else False
-
-    if not reached_due_time and not reached_max_count:
-        reactor.callLater(delay, run, spider, queue, settings, delay, i + 1, crawl_count, due_time)
-    else:
+    # pop runners queue if crawler ends
+    if reached_any_end_condition:
         queue.pop()
 
 
@@ -182,4 +141,3 @@ def run_wrapper3(spider, queue, settings, delay, due_time):
 def run_wrapper4(spider, queue, settings, delay, run_time):
     due_time = datetime.datetime.now() + datetime.timedelta(seconds=run_time)
     run(spider, queue, settings, delay, due_time=due_time)
-
