e443d8c686f391dd345c0c0b709ce383879245a2
skisiel <szymon.kisiel99@gmail.com>
2022-10-15 00:32:53 +0200

    Fixed continuous crawling

--- crawl.py
+++ crawl.py
@@ -1,98 +1,95 @@
-from sys import stdin
-from twisted.internet import reactor
-
-from time import sleep
-from twisted.internet import reactor, defer, task
-import signal
-
-from scrapy.crawler import CrawlerRunner, CrawlerProcess
+import datetime
+from utils.time_util import string_to_datetime, get_timezone_aware_now
+from twisted.internet import defer
+from scrapy.crawler import CrawlerProcess
+from reactor_queue import ReactorQueue
 from scrapy.utils.project import get_project_settings
-from Scraping2.spiders.fakt_spider import FaktNewsSpider
-from Scraping2.spiders.onet_spider import OnetNewsSpider
-from Scraping2.spiders.radiozet_spider import RadiozetNewsSpider
-from Scraping2.spiders.rmf24_spider import Rmf24NewsSpider
-from Scraping2.spiders.tvn24_spider import Tvn24NewsSpider
-from scrapy.exceptions import CloseSpider
-
-from multiprocessing import Process, Queue
-
-from scrapy.crawler import CrawlerRunner
-from scrapy.utils.project import get_project_settings
-from scrapy.utils.log import configure_logging
 from twisted.internet import reactor
-
+from Scraping2.spiders.test_spider1 import AuthorSpider
+from Scraping2.spiders.test_spider2 import QuotesSpider
+from scrapy.settings import Settings
+from scrapy import Spider
 from utils.websites_util import website_name_to_crawler
 
 
-# Enable logging for CrawlerRunner
-# configure_logging()
+@defer.inlineCallbacks
+def run(spider: Spider,
+        queue: ReactorQueue,
+        settings: Settings,
+        delay: int = 60,
+        crawl_count: int = None,
+        due_time: datetime = None,
+        i: int = 0):
 
-def startup_message():
-    print("STARTING REACTOR...")
+    # check conditions
+    if due_time is not None:
+        now_delayed = get_timezone_aware_now() + datetime.timedelta(seconds=delay)
+        reached_due_time = now_delayed > due_time
+    else:
+        reached_due_time = False
+
+    if crawl_count is not None:
+        reached_max_count = i + 1 >= crawl_count
+    else:
+        reached_max_count = False
+
+    reached_any_end_condition = reached_due_time or reached_max_count
+
+    # schedule next crawl
+    if not reached_any_end_condition:
+        reactor.callLater(delay, run, spider, queue, settings, delay, crawl_count, due_time, i + 1)
+
+    # log crawl start
+    print(f"[{datetime.datetime.now().strftime('%m/%d/%Y, %H:%M:%S.%f')}] {spider.name} run {i}")
+
+    # run crawler
+    runner = CrawlerProcess(settings)
+    yield runner.crawl(spider)
+
+    # pop runners queue if crawler ends
+    if reached_any_end_condition:
+        queue.pop()
 
 
-def shutdown_message():
-    print("STOPPING REACTOR...")
+# run infinitely
+def run_wrapper(spider, queue, settings, delay):
+    run(spider, queue, settings, delay)
 
 
-class CrawlerRunnerProcess(Process):
-    def __init__(self, spider, *args):
-        Process.__init__(self)
+# run n times
+def run_wrapper2(spider: Spider, queue: ReactorQueue, settings: Settings, delay: int, crawl_count: int):
+    run(spider, queue, settings, delay, crawl_count=crawl_count)
+
+
+# run to due time
+def run_wrapper3(spider, queue, settings, delay, due_time):
+    run(spider, queue, settings, delay, due_time=due_time)
+
+
+# run for x seconds
+def run_wrapper4(spider, queue, settings, delay, run_time):
+    due_time = datetime.datetime.now() + datetime.timedelta(seconds=run_time)
+    run(spider, queue, settings, delay, due_time=due_time)
+
+
+def crawl_websites(websites: list[str], due_time: str = None, crawl_count: int = None):
+    queue = ReactorQueue(len(websites))
+    for website in websites:
+        # get settings
         settings = get_project_settings()
         settings.set("FEEDS", {
-            f"data/test2/{spider.website}.jsonl": {"format": "jsonlines", "encoding": "utf8"},
+            f"data/test66/{website}.jsonl": {"format": "jsonlines", "encoding": "utf8"},
         })
-        self.runner = CrawlerRunner(settings)
-        self.spider = spider
-        self.args = args
+        settings.set("LOG_ENABLED", False)
 
-    def run(self):
-        deferred = self.runner.crawl(self.spider, self.args)
-        deferred.addBoth(lambda _: reactor.stop())
-        print(f"Is running: {reactor.running}")
-        reactor.run(installSignalHandlers=False)
+        crawler = website_name_to_crawler(website)
+        if crawler is None:
+            raise Exception(f"Website \"{website}\" does not exist")
 
+        delay = settings.get(crawler.delay_setting_name)
 
-# The wrapper to make it run multiple spiders, multiple times
-def run_spider(spider, *args):
-    # runner = CrawlerRunner(get_project_settings())
-    #
-    # runner.start()
-    # runner.join()
-    runner = CrawlerProcess(get_project_settings())
-    deferred = runner.crawl(spider, args)
-    reactor.callLater(30, run_spider, spider, args)
-    # deferred.callLater(30, run_spider, spider, args)
-    runner.start()
-    runner.join()
+        # run
+        run_wrapper(crawler, queue, settings, delay)
+        # run(crawler, queue, settings, delay, due_time=due_time, crawl_count=crawl_count)
+    reactor.run()
 
-
-def crawl(website_name):
-    settings = get_project_settings()
-    settings.set("FEEDS", {
-        f"data/test2/{website_name}.jsonl": {"format": "jsonlines", "encoding": "utf8"},
-    })
-
-    crawler = website_name_to_crawler(website_name)
-    if crawler is None:
-        raise Exception(f"Website \"{website_name}\" does not exist")
-    delay = settings.get(crawler.delay_setting_name)
-
-    run_spider(crawler)
-    #
-    # print("debug")
-    # print(f"Debug:\ndelay_setting_name = {crawler.delay_setting_name}\ndelay = {delay}")
-    # delay = 100
-    #
-    # deferred = task.LoopingCall(reactor.callLater, 0, run_spider, crawler)
-    # deferred.start(delay)
-
-
-def crawl_websites(websites: list[str]):
-    for website in websites:
-        crawl(website)
-
-    reactor.addSystemEventTrigger('during', 'startup', startup_message)
-    reactor.addSystemEventTrigger('during', 'shutdown', shutdown_message)
-    # reactor.callInThread(cmdloop)
-    # reactor.run()
