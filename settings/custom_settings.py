7dcd0832f41b81b76e15e591e3880728ecaf1fac
skisiel <szymon.kisiel99@gmail.com>
2022-11-16 19:19:57 +0100

    Fixed project settings

--- settings/custom_settings.py
+++ settings/custom_settings.py
@@ -1,5 +1,15 @@
-from scrapy.utils.project import get_project_settings
+import os
+
+from scrapy.settings import Settings
 from utils.websites_util import website_name_to_data_filename
+
+
+def get_project_settings():
+    settings = Settings()
+    os.environ['SCRAPY_SETTINGS_MODULE'] = 'news_scraping.settings'
+    settings_module_path = os.environ['SCRAPY_SETTINGS_MODULE']
+    settings.setmodule(settings_module_path, priority='project')
+    return settings
 
 
 def get_custom_project_settings(website: str):
@@ -10,4 +20,6 @@ def get_custom_project_settings(website: str):
             f"data/{filename}": {"format": "jsonlines", "encoding": "utf8"},
         })
     settings.set("LOG_ENABLED", False)
+    # settings.set("COOKIES_DEBUG", True)
+
     return settings
